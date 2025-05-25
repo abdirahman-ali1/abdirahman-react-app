01b127c11d0fd62fa1dc49f2a5893bb7b19562a7
Abdirahman ali <194543183+abdirahman-ali1@users.noreply.github.com>
2025-05-25 02:08:08 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Greeting from './components/Greeting';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <>
+    <Greeting name= "Abdirahman" surname= "Ali"></Greeting>
+      <Header></Header>
+      
+    </>
+  );
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>This is my first React Application</h1>
+    </div>
+  );
+}
+
+export default Header
