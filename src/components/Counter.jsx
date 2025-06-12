1562a8c9ee6c16cfa1db0cde65791b351ba81862
Abdirahman ali <194543183+abdirahman-ali1@users.noreply.github.com>
2025-06-12 22:36:35 +0100

    i have added colour to the text count

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
 function Counter() {
-    const [count, setcount] = useState(0);
+    const [count, setcount] = useState(0)
+    
 
     return (
         <div style= {{backgroundColor:"yellow" , padding:"20px"}}>
-            <h2>Count: {count}</h2>
+            <h2 style= {{color:"red"}}>Count: {count}</h2>
             <button onClick={()=> setcount(count + 1)}>Increase</button>
             <button onClick={()=> setcount(count -1)}>Decrease</button>
         </div>
